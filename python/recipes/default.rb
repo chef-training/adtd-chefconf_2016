d6824ac8fe7ec7f074493b351df73dd8cdf97f4b
Franklin Webber <franklin.webber@gmail.com>
2016-07-09 03:22:32 -0500

    Completes Scenario 04
    * Implements the custom resource
    * Uses the ark resource

--- python/recipes/default.rb
+++ python/recipes/default.rb
@@ -10,21 +10,8 @@ else
   package %w[openssl-devel]
 end
 
-remote_file '/tmp/Python-3.4.4.tgz' do
-  source 'https://www.python.org/ftp/python/3.4.4/Python-3.4.4.tgz'
-
-  notifies :run, 'execute[extract python]', :immediately
-end
-
-execute 'extract python' do
-  command 'tar xzf /tmp/Python-3.4.4.tgz'
-  cwd '/tmp'
-  action :nothing
-  notifies :run, 'execute[python build and install]', :immediately
-end
-
-execute 'python build and install' do
-  command './configure && make install'
-  cwd '/tmp/Python-3.4.4'
-  action :nothing
+ark 'python' do
+  version '3.4.4'
+  url 'https://www.python.org/ftp/python/3.4.4/Python-3.4.4.tgz'
+  action :install_with_make
 end
--- python/resources/pip.rb
+++ python/resources/pip.rb
@@ -1,3 +1,9 @@
 resource_name :pip
 
-# TODO: create a pip resource
+property :package_name, String, name_attribute: true
+
+action :install do
+  pip_executable = '/usr/local/bin/pip3'
+
+  execute "#{pip_executable} install #{package_name}"
+end
