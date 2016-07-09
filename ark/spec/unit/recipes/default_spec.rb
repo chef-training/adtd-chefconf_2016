ce1ff660c2c93b7469a8bfdba03b46dabbee329b
Franklin Webber <franklin.webber@gmail.com>
2016-07-09 03:04:55 -0500

    Completes Scenario 01
    * Applying let
    * Applying shared_context

--- ark/spec/unit/recipes/default_spec.rb
+++ ark/spec/unit/recipes/default_spec.rb
@@ -1,29 +1,40 @@
 require 'spec_helper'
 
 describe 'ark::default' do
-  context 'when no attributes are specified, on an unspecified platform' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new
-      runner.converge(described_recipe)
-    end
+  let(:chef_run) do
+    runner = ChefSpec::SoloRunner.new(node_attributes)
+    runner.converge(described_recipe)
+  end
 
+  let(:node_attributes) do
+    {}
+  end
+
+  shared_examples 'installs packages' do
     it 'installs necessary packages' do
-      expect(chef_run).to install_package('libtool')
-      expect(chef_run).to install_package('autoconf')
-      expect(chef_run).to install_package('unzip')
-      expect(chef_run).to install_package('rsync')
-      expect(chef_run).to install_package('make')
-      expect(chef_run).to install_package('gcc')
-      expect(chef_run).to install_package('autogen')
+      installed_packages.each { |name| expect(chef_run).to install_package(name) }
+    end
+  end
+
+  shared_examples 'does not install packages' do
+    it 'does not install packages' do
+      packages_not_installed.each { |name| expect(chef_run).not_to install_package(name) }
+    end
+  end
+
+  context 'when no attributes are specified, on an unspecified platform' do
+
+    let(:installed_packages) do
+      %w[ libtool autoconf unzip rsync make gcc autogen ]
     end
 
-    it "does not install the gcc-c++ package" do
-      expect(chef_run).not_to install_package("gcc-c++")
+    it_behaves_like 'installs packages'
+
+    let(:packages_not_installed) do
+      %w[ gcc-c++ seven_zip ]
     end
 
-    it "does not include the seven_zip recipe" do
-      expect(chef_run).not_to include_recipe("seven_zip")
-    end
+    it_behaves_like 'does not install packages'
 
     it "apache mirror" do
       attribute = chef_run.node['ark']['apache_mirror']
@@ -52,60 +63,39 @@ describe 'ark::default' do
   end
 
   context 'when no attributes are specified, on CentOS' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'centos', version: '6.7')
-      runner.converge(described_recipe)
+    let(:node_attributes) do
+      { platform: 'centos', version: '6.7' }
     end
 
-    it 'installs necessary packages' do
-      expect(chef_run).to install_package('libtool')
-      expect(chef_run).to install_package('autoconf')
-      expect(chef_run).to install_package('unzip')
-      expect(chef_run).to install_package('rsync')
-      expect(chef_run).to install_package('make')
-      expect(chef_run).to install_package('gcc')
-      expect(chef_run).to install_package('xz-lzma-compat')
-      expect(chef_run).to install_package('bzip2')
-      expect(chef_run).to install_package('tar')
+    let(:installed_packages) do
+      %w[ libtool autoconf unzip rsync make gcc xz-lzma-compat bzip2 tar ]
     end
+
+    it_behaves_like 'installs packages'
   end
 
   context 'when no attributes are specified, on Debian' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', platform_family: 'debian', version: '14.04')
-      runner.converge(described_recipe)
+    let(:node_attributes) do
+      { platform: 'ubuntu', platform_family: 'debian', version: '14.04' }
     end
 
-    it 'installs necessary packages' do
-      expect(chef_run).to install_package('libtool')
-      expect(chef_run).to install_package('autoconf')
-      expect(chef_run).to install_package('unzip')
-      expect(chef_run).to install_package('rsync')
-      expect(chef_run).to install_package('make')
-      expect(chef_run).to install_package('gcc')
-      expect(chef_run).to install_package('autogen')
-      expect(chef_run).to install_package('shtool')
-      expect(chef_run).to install_package('pkg-config')
+    let(:installed_packages) do
+      %w[ libtool autoconf unzip rsync make gcc autogen shtool pkg-config ]
     end
 
+    it_behaves_like 'installs packages'
   end
 
   context 'when no attributes are specified, on FreeBSD' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'freebsd', version: '10.2')
-      runner.converge(described_recipe)
+    let(:node_attributes) do
+      { platform: 'freebsd', version: '10.2' }
     end
 
-    it 'installs necessary packages' do
-      expect(chef_run).to install_package('libtool')
-      expect(chef_run).to install_package('autoconf')
-      expect(chef_run).to install_package('unzip')
-      expect(chef_run).to install_package('rsync')
-      expect(chef_run).to install_package('gmake')
-      expect(chef_run).to install_package('gcc')
-      expect(chef_run).to install_package('autogen')
-      expect(chef_run).to install_package('gtar')
+    let(:installed_packages) do
+      %w[ libtool autoconf unzip rsync gmake gcc autogen gtar ]
     end
+
+    it_behaves_like 'installs packages'
 
     it "tar binary" do
       attribute = chef_run.node['ark']['tar']
@@ -114,19 +104,15 @@ describe 'ark::default' do
   end
 
   context 'when no attributes are specified, on Mac OSX' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'mac_os_x', version: '10.11.1')
-      runner.converge(described_recipe)
+    let(:node_attributes) do
+      { platform: 'mac_os_x', version: '10.11.1' }
     end
 
-    it 'does not install packages' do
-      expect(chef_run).not_to install_package('libtool')
-      expect(chef_run).not_to install_package('autoconf')
-      expect(chef_run).not_to install_package('unzip')
-      expect(chef_run).not_to install_package('rsync')
-      expect(chef_run).not_to install_package('make')
-      expect(chef_run).not_to install_package('gcc')
+    let(:packages_not_installed) do
+      %w[ libtool autoconf unzip rsync make gcc ]
     end
+
+    it_behaves_like 'does not install packages'
 
     it "tar binary" do
       attribute = chef_run.node['ark']['tar']
@@ -135,40 +121,27 @@ describe 'ark::default' do
   end
 
   context 'when no attributes are specified, on RHEL' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'redhat', platform_family: 'rhel', version: '6.5')
-      runner.converge(described_recipe)
+    let(:node_attributes) do
+      { platform: 'redhat', platform_family: 'rhel', version: '6.5' }
     end
 
-    it 'installs necessary packages' do
-      expect(chef_run).to install_package('libtool')
-      expect(chef_run).to install_package('autoconf')
-      expect(chef_run).to install_package('unzip')
-      expect(chef_run).to install_package('rsync')
-      expect(chef_run).to install_package('make')
-      expect(chef_run).to install_package('gcc')
-      expect(chef_run).to install_package('xz-lzma-compat')
-      expect(chef_run).to install_package('bzip2')
-      expect(chef_run).to install_package('tar')
+    let(:installed_packages) do
+      %w[ libtool autoconf unzip rsync make gcc xz-lzma-compat bzip2 tar ]
     end
+
+    it_behaves_like 'installs packages'
   end
 
   context 'when no attributes are specified, on SmartOS' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'smartos', version: '5.11')
-      runner.converge(described_recipe)
+    let(:node_attributes) do
+      { platform: 'smartos', version: '5.11' }
     end
 
-    it 'installs necessary packages' do
-      expect(chef_run).to install_package('libtool')
-      expect(chef_run).to install_package('autoconf')
-      expect(chef_run).to install_package('unzip')
-      expect(chef_run).to install_package('rsync')
-      expect(chef_run).to install_package('make')
-      expect(chef_run).to install_package('gcc')
-      expect(chef_run).to install_package('gtar')
-      expect(chef_run).to install_package('autogen')
+    let(:installed_packages) do
+      %w[ libtool autoconf unzip rsync make gcc gtar autogen ]
     end
+
+    it_behaves_like 'installs packages'
 
     it "tar binary" do
       attribute = chef_run.node['ark']['tar']
@@ -177,24 +150,15 @@ describe 'ark::default' do
   end
 
   context 'when no attributes are specified, on Windows' do
-    let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'windows', version: '2012R2')
-      runner.converge(described_recipe)
+    let(:node_attributes) do
+      { platform: 'windows', version: '2012R2' }
     end
 
-    it 'does not installs packages' do
-      expect(chef_run).not_to install_package('libtool')
-      expect(chef_run).not_to install_package('autoconf')
-      expect(chef_run).not_to install_package('unzip')
-      expect(chef_run).not_to install_package('rsync')
-      expect(chef_run).not_to install_package('make')
-      expect(chef_run).not_to install_package('gmake')
-      expect(chef_run).not_to install_package('gcc')
-      expect(chef_run).not_to install_package('autogen')
-      expect(chef_run).not_to install_package('xz-lzma-compat')
-      expect(chef_run).not_to install_package('bzip2')
-      expect(chef_run).not_to install_package('tar')
+    let(:packages_not_installed) do
+      %w[ libtool autoconf unzip rsync make gmake gcc autogen xz-lzma-compat bzip2 tar ]
     end
+
+    it_behaves_like 'does not install packages'
 
     it "tar binary" do
       attribute = chef_run.node['ark']['tar']
